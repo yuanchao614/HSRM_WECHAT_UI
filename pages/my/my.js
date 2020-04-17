b70d74b0d3d0a777ee7c8ed4fb458c8e24e77ced
wang.jiang <2428306489@qq.com>
2020-04-17 16:12:39 +0800

    新增订单删除功能

--- pages/my/my.js
+++ pages/my/my.js
@@ -11,14 +11,14 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     var that = this;
     wx.setNavigationBarTitle({
       title: '我的订单',
     });
     // 获取屏幕宽高
     wx.getSystemInfo({
-      success: function (res) {
+      success: function(res) {
         console.log(res)
         that.setData({
           winWidth: res.windowWidth,
@@ -32,53 +32,53 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
 
-  getData: function () {
+  getData: function() {
     const mobile = wx.getStorageSync('loginData').data.userInfo.phoneNum;
     wx.request({
       url: `http://127.0.0.1:3000/api/highSpeedOrderManagement/queryByPhoneNum?phoneNum=${mobile}`,
@@ -96,7 +96,22 @@ Page({
     })
   },
 
-  backTicket: function () {
+  deleteOrder: function(event) {
+    const param = event.currentTarget.dataset.id;
+    console.log(param)
+      wx.request({
+        url: `http://127.0.0.1:3000/api/highSpeedOrderManagement/deleteOrder?id=${param}`,
+        header: {
+          'content-type': 'application/json', // 默认值
+        },
+        success: (res) => { // 箭头函数内部没有this
+          console.log(res);
+          this.getData();
+        }
+      })
+  },
+
+  backTicket: function() {
     wx.redirectTo({
       url: '../tickets/tickets',
     })
